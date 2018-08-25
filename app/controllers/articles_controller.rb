a806962dd63789dedaf9df0a59d154783d09a6e5
brandnamewater <brandnamewater@gmail.com>
2018-08-25 05:00:10 -0400

    added charector requirement length for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,20 +19,22 @@ class ArticlesController < ApplicationController
 
 
   def new
-
+    @article = Article.new
   end
 
   def create
-  #  render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
+    if @article.save
     redirect_to @article
+  else
+    render 'new'
   end
+end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
+
     redirect_to articles_path
   end
 
