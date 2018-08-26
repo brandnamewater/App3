d80c11d11fd5e7907f6545052f7c69aa99347739
brandnamewater <brandnamewater@gmail.com>
2018-08-26 04:39:30 -0400

    added stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  #nav name white if active
+  #takes arguments: 'link', 'what page on', 'url', 'change font color'
+  def active_link_to(nav_link, cntrl, action, url, color)
+    if controller_name == cntrl && action_name == action
+      return link_to nav_link, url, id: color
+    else
+      return link_to nav_link, url, class: color
+    end
+  end
+
+
+
+
+
 end
